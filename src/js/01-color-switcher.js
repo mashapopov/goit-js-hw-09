2735552cdc132915fae64cce8c31e43a427d73bc
Maria Popova <0979867251m@gmail.com>
2023-02-05 18:58:47 +0200

    color

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,24 @@
+const startBtn = document.querySelector(`[data-start]`);
+const stopBtn = document.querySelector(`[data-stop]`);
+const body = document.body;
+let timerId = null;
+
+stopBtn.setAttribute('disabled', true);
+startBtn.addEventListener("click", () => {
+  stopBtn.removeAttribute('disabled');
+    startBtn.setAttribute('disabled', true);
+  timerId = setInterval(() => {
+     let randomColor = getRandomHexColor();
+      body.style.backgroundColor = `${randomColor}`;
+  }, 1000);
+});
+
+stopBtn.addEventListener("click", () => {
+  startBtn.removeAttribute('disabled');
+  stopBtn.setAttribute('disabled', true);
+  clearInterval(timerId);
+});
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
